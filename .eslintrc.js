7876058d33417c8fd343ca8224f8adbbcf9d32c5
Serhii Yunnikov <yunnikov@icloud.com>
2023-04-21 16:18:03 +0300

    phone_catalog

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,10 @@ module.exports = {
     'react/prop-types': 0,
     'react/self-closing-comp': 0,
     'react/display-name': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [0],
+    'import/extensions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
 
     // JavaScript
     semi: 0,
@@ -15,6 +19,8 @@ module.exports = {
     'no-unused-vars': 0,
 
     // TypeScript
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
